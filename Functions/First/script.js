1868ee34ac28b4c24e1104a36e8f41484518c7eb
chiaraintech <chiaraintech@gmail.com>
2021-03-24 08:48:43 +0000

    Higher-order functions example

--- Functions/First/script.js
+++ Functions/First/script.js
@@ -2,6 +2,7 @@
 //ES5, we'd use short circuiting. ES6 we assign the default values in the parameters (can also be calculations).
 //However we can't skip a default parameter when calling the function unless you set it to undefined.
 
+//1. Default parameters.
 const bookingArray = [];
 const createBooking = function(flightNum, numPassengers = 1, price = 200) {
         const booking = {
@@ -15,6 +16,7 @@ const createBooking = function(flightNum, numPassengers = 1, price = 200) {
 
 createBooking('LH123', 4, 190);
 
+//2. How passing arguments work (value VS reference)
 const flight = 'JFK345'
 const chiara = {
     name: 'Chiara',
@@ -24,7 +26,7 @@ const chiara = {
 const checkIn = function(flightNum, passenger) {
     flightNum = 'LAX890';
     passenger.name = 'Miss. ' + passenger.name;
-    if (passenger.passport === 789445) { alert ('check in.')} else { alert('wrong passport')};
+    //if (passenger.passport === 789445) { alert ('check in.')} else { alert('wrong passport')};
 }
 checkIn(flight, chiara);        
 //flight is a primitive type. the parameter contains a copy of the original value.
@@ -37,3 +39,19 @@ const newPassport = function(person) {
 newPassport(chiara);
 checkIn(flight, chiara);
 //We have two functions manipulating the same object, so prone to errors.
+
+//3.Higher-order functions.
+
+const oneWord = function(string) {
+    return string.replace(/ /g, '').toLowerCase();
+}
+const upperFirstWord = function(string) {
+    const [firstWord, ...others] = string.split(' ');
+    return [firstWord.toUpperCase(), ...others].join(' ');
+}
+const transform = function(string, func) {
+    console.log(`Original string: ${string}`);
+    console.log(`Transformed string: ${func(string)}`);
+    console.log(`Transformed by: ${func.name}`);
+}
+transform('Javascript is the best!', upperFirstWord);
