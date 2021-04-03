7e68f7dc9a928feb9748fa3db26d97870ca0e655
chiaraintech <chiaraintech@gmail.com>
2021-04-03 15:26:35 +0100

    5 methods theory.

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,76 +1,48 @@
 'use strict';
+//Array methods: simply methods which are accessible to arrays, and objects.
+//Arrays are objects which get access to special built-in methods.
+let arr = ['a', 'b', 'c', 'd', 'e'];
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
+/////////////---SLICE----//////////
+//You can extract part of any array without changing the original one.
+//It returns a copy of the array just with the extracted parts.
+//First number is starting parameter, the second one is the last.
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -1));
 
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
+//We can also use the method to create a shallow copy of the array.
+console.log(arr.slice());
+//Same result if I use the spread operator:
+console.log([...arr]);
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+/////////////---SPLICE----//////////
+//It works almost in the same way as slice.
+//Fundamental difference: it does mutate the original array.
+//After I splice an array, if I look at the original array, it'll be modified.
+console.log(arr.splice(2));
+arr.splice(-1); //Common use: remove last element.
+console.log(arr);
 
-const accounts = [account1, account2, account3, account4];
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+/////////////---REVERSE----//////////
+//Reverse mutates the original array.
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+let arr2 = ['f', 'i', 'h', 'g', 'j'];
+console.log(arr2.reverse());
+console.log(arr2)
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+/////////////---CONCAT----//////////
+//Used to concatenate two arrays.
+const letters = arr1.concat(arr2)
+console.log(letters)
+//Same result if I use the spread operator:
+console.log([...arr1, ...arr2]);
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+/////////////---Join----//////////
+//If I want to join strings with a separator of my choice.
+console.log(letters.join('-'))
