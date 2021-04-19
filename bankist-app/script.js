8160eb5bf92f4fbefd5629aeffebcc99e7e975b1
chiaraintech <chiaraintech@gmail.com>
2021-04-19 17:11:29 +0100

    First functionality added via HTML template literals

--- bankist-app/script.js
+++ bankist-app/script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//Instead of working with global variables, pass the variables that that function really needs.
+const displayMovements = function (movements) {
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
